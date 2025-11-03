bd9499f46ee36794dcbad72fb44ac84a0ab1c2ba
omaraskar <tareqsyria741@gmail.com>
2025-11-03 11:42:31 +0300

    edited max speed in location history to default to float 3.0

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -121,7 +121,7 @@ class CarController extends Controller
             'car_name' => $car->car_name,
             'date' => $date,
             'total_distance' => round($totalDistance, 2),
-            'max_speed' => (double)($locations->max('speed') ?? 0), // Use (double) cast            'locations_count' => $locations->count(),
+            'max_speed' => (float)($locations->max('speed') ?? 0.0), // Use (double) cast            'locations_count' => $locations->count(),
             'locations' => GpsDataResource::collection($locations)
         ]);
     }
