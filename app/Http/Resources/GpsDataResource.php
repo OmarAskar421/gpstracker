f87198342f7d9754ecff03de46f871a4882d92d4
omaraskar <tareqsyria741@gmail.com>
2026-03-13 19:31:20 +0300

    added sattelite count to json of live update to android

--- app/Http/Resources/GpsDataResource.php
+++ app/Http/Resources/GpsDataResource.php
@@ -1,9 +1,7 @@
-<?php
 // app/Http/Resources/GpsDataResource.php
 
 namespace App\Http\Resources;
 
-// Make sure Carbon is imported
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -13,18 +11,19 @@ class GpsDataResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-    'latitude' => (float) $this->latitude,
-    'longitude' => (float) $this->longitude,
-    'speed' => $this->speed ? (float) $this->speed : null,
-    'heading' => $this->heading ? (float) $this->heading : null,
-    'altitude' => $this->altitude ? (float) $this->altitude : null,
-    'accuracy' => $this->accuracy ? (float) $this->accuracy : null,
-    'ignition' => $this->ignition,              // CHANGED from battery
-    'door_open' => $this->door_open,
-    'fuel_cutoff' => $this->fuel_cutoff,
-    'voltage' => $this->voltage ? (float) $this->voltage : null,    // ADD
-    'snr' => $this->snr ? (float) $this->snr : null,                // ADD
-    'timestamp' => $this->recorded_at ? Carbon::parse($this->recorded_at)->toISOString() : null,
-];
+            'latitude' => (float) $this->latitude,
+            'longitude' => (float) $this->longitude,
+            'speed' => $this->speed ? (float) $this->speed : null,
+            'heading' => $this->heading ? (float) $this->heading : null,
+            'altitude' => $this->altitude ? (float) $this->altitude : null,
+            'accuracy' => $this->accuracy ? (float) $this->accuracy : null,
+            'satellite_count' => $this->satellite_count ? (int) $this->satellite_count : null,  // ← ADD THIS LINE
+            'ignition' => $this->ignition,
+            'door_open' => $this->door_open,
+            'fuel_cutoff' => $this->fuel_cutoff,
+            'voltage' => $this->voltage ? (float) $this->voltage : null,
+            'snr' => $this->snr ? (float) $this->snr : null,
+            'timestamp' => $this->recorded_at ? Carbon::parse($this->recorded_at)->toISOString() : null,
+        ];
     }
 }
